d32ed12abb34c60b4321ff8d0182d5a0710e819d
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-05-17 01:48:53 +0300

    ADD: Loading lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { Suspense } from 'react';
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router';
-import TweetsPage from './views/Home/Tweets/TweetsPage';
-import HomePage from './views/Home/HomePage';
-import NotFoundPage from './views/Home/NotFound/NotFoundPage';
+
+const HomeLazyPage = lazy(() => import('./views/Home'));
+const TweetsLazyPage = lazy(() => import('./views/Tweets/TweetsPage'));
+const NotFoundLazyPage = lazy(() => import('./views/NotFound/NotFoundPage'));
 
 const App = () => {
 	return (
 		<>
 			<Suspense fallback={<div>Loading...</div>}>
 				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/tweets" element={<TweetsPage />} />
-					<Route path="*" element={<NotFoundPage />} />
+					<Route path="/" element={<HomeLazyPage />} />
+					<Route path="/tweets" element={<TweetsLazyPage />} />
+					<Route path="*" element={<NotFoundLazyPage />} />
 				</Routes>
 			</Suspense>
 		</>
