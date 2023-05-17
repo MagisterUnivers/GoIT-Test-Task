dcb8485913a9c033f81b238020a75a320dbe6660
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-05-17 20:40:15 +0300

    NotFound bonus

--- src/components/Notfound/notfound.jsx
+++ src/components/Notfound/notfound.jsx
@@ -1,3 +1,13 @@
-const NotFound = () => {};
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+	return (
+		<>
+			{' '}
+			<div>404 PAGE NOT FOUND</div>
+			<Link to="/">Home</Link>
+		</>
+	);
+};
 
 export default NotFound;
